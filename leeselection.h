64050c71791332b6e68862be99c846d15a1ac65b
Will <105031962+Liyourong@users.noreply.github.com>
2022-05-23 21:27:23 -0400

    Create leeselection.h

---
+++ leeselection.h
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+void print (int);
+void print (string);
+
+void printl (int);
+void printl (string);
+
+void printlt (int);
+void printlt (string);
+
+void printt (int);
+void printt(string );
+
+int add(int, int);
+int random(int, int);
+
+int in (string);
+string ins(string);
